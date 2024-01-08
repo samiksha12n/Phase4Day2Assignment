37ab1eefcc5ef7ca2044989fdee8f32aad4dcc46
Samiksha Nesarkar <90084670+samiksha12n@users.noreply.github.com>
2024-01-08 16:03:37 +0530

    Add files via upload

---
+++ SignIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitSignInLib
+{
+    public class SignIn
+    {
+        public string Authenticate(string username, string password)
+        {
+            string result = "";
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                result = "Please provide username and password";
+            }
+            else
+            {
+                if (username == "sam" && password == "sam@1256")
+                {
+                    result = "Authentication Pass";
+                }
+                else
+                {
+                    result = "Authentication Fail";
+                }
+            }
+            return result;
+        }
+    }
+}
